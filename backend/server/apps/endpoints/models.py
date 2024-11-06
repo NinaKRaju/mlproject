d72696503a0a3342fa6214aba821b975e417af7c
NinaKRaju <ninakraju@gmail.com>
2024-11-06 14:21:05 -0700

    build django models

---
+++ backend/server/apps/endpoints/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+class Endpoint(models.Model):
+# keeps information about my endpoint
+    name = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    
+class MLAlgorithm(models.Model):
+# keeps information about ML algorithms used in the service
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    code = models.CharField(max_length=50000)
+    version = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+    
+class MLAlgorithmStatus(models.Model):
+# keeps information about ML algorithm statuse. ex: testing to production state
+    status = models.CharField(max_length=128)
+    active = models.BooleanField()
+    created_by = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name
+    
+class MLRequest(models.Model):
+# keeps information about all requests to ML algorithms
+    input_data = models.CharField(max_length=10000)
+    full_response = models.CharField(max_length=10000)
+    response = models.CharField(max_length=10000)
+    feedback = models.CharField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
+    
+    
