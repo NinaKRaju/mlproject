71737b0dd0bffddac90fee27ecfcd8561694d842
NinaKRaju <ninakraju@gmail.com>
2024-11-14 14:20:39 -0700

    serve ml models

---
+++ backend/server/apps/ml/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from apps.ml.rain_classifier.svc import SVC
+
+class MLTests(TestCase):
+    def test_svc_algorithm(self):
+        input_data = {
+            "day": 1,
+            "pressure": 1025.9,
+            "maxtemp": 19.9,
+            "temperature": 18.3,
+            "mintemp": 16.8,
+            "dewpoint": 13.1,
+            "humidity": 72,
+            "cloud": 49,
+            "sunshine": 9.3,
+            "winddirection": 80,
+            "windspeed": 26.3,
+        }
+        my_alg = SVC()
+        response = my_alg.compute_prediction(input_data)
+        self.assertEqual('OK', response['status'])
+        self.assertTrue('label' in response)
+        self.assertEqual('Rain', response['label'])
