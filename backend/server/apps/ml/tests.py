077c23e54dbe4ab488ca33c8feb89f0dc36ad0a3
NinaKRaju <ninakraju@gmail.com>
2024-11-25 14:25:14 -0700

    income classifier ml

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -1,46 +1,48 @@
 from django.test import TestCase
 
-from apps.ml.student_classifier.linreg import LinearRegression
+from apps.ml.income_classifier.random_forest import RandomForestClassifier
+import inspect
+from apps.ml.registry import MLRegistry
 
 class MLTests(TestCase):
-    def test_linreg_algorithm(self):
+    def test_rf_algorithm(self):
         input_data = {
-            "school": "GP",
-            "sex": "F",
-            "age": 18,
-            "address": "U",
-            "famsize": "GT3",
-            "Pstatus":"A",
-            "Medu":4,
-            "Fedu":4,
-            "Mjob":"at_home",
-            "Fjob":"teacher",
-            "reason":"course",
-            "guardian":"mother",
-            "traveltime":2,
-            "studytime":2,
-            "failures":0,
-            "schoolsup":"yes",
-            "famsup": "no",
-            "paid": "no",
-            "activities": "no",
-            "nursery":"yes",
-            "higher":"yes",
-            "internet":"no",
-            "romantic":"no",
-            "famrel":4,
-            "freetime":3,
-            "goout":4,
-            "Dalc":1,
-            "Walc":1,
-            "health":3,
-            "absences":6,
-            "G1":"5",
-            "G2":"6",
-            "G3":6
+            "age": 37,
+            "workclass": "Private",
+            "fnlwgt": 34146,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Craft-repair",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 68,
+            "native-country": "United-States"
         }
-        my_alg = LinearRegression()
+        my_alg = RandomForestClassifier()
         response = my_alg.compute_prediction(input_data)
-#        self.assertEqual('OK', response['status'])
- #       self.assertTrue('prediction' in response)
- #       self.assertEqual(6, response['prediction'])
+        self.assertEqual('OK', response['status'])
+        self.assertTrue('label' in response)
+        self.assertEqual('<=50K', response['label'])
+        
+    def test_registry(self):
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "income_classifier"
+        algorithm_object = RandomForestClassifier()
+        algorithm_name = "random forest"
+        algorithm_status = "production"
+        algorithm_version = "0.0.1"
+        algorithm_owner = "Piotr"
+        algorithm_description = "Random Forest with simple pre- and post-processing"
+        algorithm_code = inspect.getsource(RandomForestClassifier)
+        # add to registry
+        registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                    algorithm_status, algorithm_version, algorithm_owner,
+                    algorithm_description, algorithm_code)
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
+
